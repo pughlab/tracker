e073a0f66c76f9392d2b652be348c75320d69e05
Stuart Watt <stuart@morungos.com>
2015-03-11 12:40:56 -0400

    Added an interceptor, and now we can pick out the Shiro information.

--- src/main/java/ca/uhnresearch/pughlab/tracker/sockets/TrackerSocketHandler.java
+++ src/main/java/ca/uhnresearch/pughlab/tracker/sockets/TrackerSocketHandler.java
@@ -1,17 +1,34 @@
 package ca.uhnresearch.pughlab.tracker.sockets;
 
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.env.WebEnvironment;
+import org.apache.shiro.web.mgt.WebSecurityManager;
+import org.apache.shiro.web.util.WebUtils;
 import org.atmosphere.config.service.Disconnect;
 import org.atmosphere.config.service.Heartbeat;
 import org.atmosphere.config.service.ManagedService;
 import org.atmosphere.config.service.Ready;
 import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.FrameworkConfig;
+import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor;
+import org.atmosphere.interceptor.HeartbeatInterceptor;
+import org.atmosphere.interceptor.ShiroInterceptor;
+import org.atmosphere.interceptor.SuspendTrackerInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-@ManagedService(path = "/events")
+import javax.servlet.ServletContext;
+
+@ManagedService(path = "/events",
+                interceptors = {
+		AtmosphereResourceLifecycleInterceptor.class,
+		HeartbeatInterceptor.class,
+		SuspendTrackerInterceptor.class,
+		ShiroInterceptor.class
+})
 public class TrackerSocketHandler {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -28,7 +45,11 @@ public class TrackerSocketHandler {
      */
     @Ready
     public void onReady(final AtmosphereResource r) {
+
         logger.info("Browser {} connected", r.uuid());
+        
+        Subject subject = (Subject) r.getRequest().getAttribute(FrameworkConfig.SECURITY_SUBJECT);
+        logger.info("Subject: {}", subject.getPrincipal());
     }
 
     /**
