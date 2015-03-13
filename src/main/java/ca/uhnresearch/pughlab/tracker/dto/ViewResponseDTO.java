d3a2c6430a308f06d4605f79f5acc99c791d5d07
Stuart Watt <stuart@morungos.com>
2015-03-13 14:59:19 -0400

    Whoa - don't override the study

--- src/main/java/ca/uhnresearch/pughlab/tracker/dto/ViewResponseDTO.java
+++ src/main/java/ca/uhnresearch/pughlab/tracker/dto/ViewResponseDTO.java
@@ -9,22 +9,11 @@ import ca.uhnresearch.pughlab.tracker.domain.Views;
 
 public class ViewResponseDTO extends StudyResponseDTO {
 
-	StudyDTO study;
 	ViewDTO view;
 
 	public ViewResponseDTO(URL url, UserDTO user, Studies s, Views v) {
 		super(url, user, s);
 		this.view = new ViewDTO(v);
-	}
-
-
-	@JsonProperty
-	public StudyDTO getStudy() {
-		return study;
-	}
-
-	public void setStudy(StudyDTO study) {
-		this.study = study;
 	}
 
 	@JsonProperty
