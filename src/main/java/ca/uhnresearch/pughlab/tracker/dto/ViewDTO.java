13c3762a23946b0fa0742c7b29b2b5387ae71067
Stuart Watt <stuart@morungos.com>
2015-04-20 15:10:23 -0400

    Added some Javadoc comments

--- src/main/java/ca/uhnresearch/pughlab/tracker/dto/ViewDTO.java
+++ src/main/java/ca/uhnresearch/pughlab/tracker/dto/ViewDTO.java
@@ -11,20 +11,32 @@ public class ViewDTO {
 		setName(v.getName());
 	}
 	
+	/**
+	 * @return the name
+	 */
 	@JsonProperty
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the permissions to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the id
+	 */
 	@JsonProperty
 	public Integer getId() {
 		return id;
 	}
 
+	/**
+	 * @param id the permissions to set
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
