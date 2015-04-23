dcad74655234a03e7a6137bf492e1679209f6399
Stuart Watt <stuart@morungos.com>
2015-04-23 16:46:57 -0400

    See #4: Added mapping from the view DTO back to a database version
    Add the schema endpoints 
    
    Task-Url: https://bitbucket.org/pughlab/tracker-java-restlet/issue/4/

--- src/main/java/ca/uhnresearch/pughlab/tracker/dto/ViewDTO.java
+++ src/main/java/ca/uhnresearch/pughlab/tracker/dto/ViewDTO.java
@@ -1,17 +1,53 @@
 package ca.uhnresearch.pughlab.tracker.dto;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ca.uhnresearch.pughlab.tracker.domain.Views;
 
 public class ViewDTO {
 	
+	private static ObjectMapper mapper = new ObjectMapper();
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	public ViewDTO() { }
 	
 	public ViewDTO(Views v) {
 		setId(v.getId());
 		setName(v.getName());
 		setDescription(v.getDescription());
+
+		String attributeOptions = v.getOptions();
+		if (attributeOptions != null) {
+			try {
+				setOptions(mapper.readValue(attributeOptions, JsonNode.class));
+			} catch (IOException e) {
+				logger.error("Error in JSON attribute options", e.getMessage());
+			}
+		}
+	}
+	
+	@JsonIgnore
+	public Views getViews() {
+		Views result = new Views();
+		result.setId(getId());
+		result.setName(getName());
+		result.setDescription(getDescription());
+		try {
+			result.setOptions(mapper.writeValueAsString(getOptions()));
+		} catch (JsonProcessingException e) {
+			logger.error("Error in JSON attribute options", e.getMessage());
+		}
+		return result;
 	}
 	
 	/**
@@ -58,8 +94,24 @@ public class ViewDTO {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	/**
+	 * @return the options
+	 */
+	@JsonProperty
+	public JsonNode getOptions() {
+		return options;
+	}
+
+	/**
+	 * @param options the options to set
+	 */
+	public void setOptions(JsonNode options) {
+		this.options = options;
+	}
 
 	private Integer id;
 	private String name;
 	private String description;
+	private JsonNode options;
 }
