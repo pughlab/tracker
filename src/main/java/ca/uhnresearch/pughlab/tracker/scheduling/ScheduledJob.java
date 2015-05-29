d24ef47fd034e0e67c9988b7937f11df7f2846ac
Stuart Watt <stuart@morungos.com>
2015-05-29 17:17:19 -0400

    Added an initial basic script system

--- src/main/java/ca/uhnresearch/pughlab/tracker/scheduling/ScheduledJob.java
+++ src/main/java/ca/uhnresearch/pughlab/tracker/scheduling/ScheduledJob.java
@@ -1,16 +1,48 @@
 package ca.uhnresearch.pughlab.tracker.scheduling;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.quartz.JobExecutionException;
 
 public class ScheduledJob  {
 	
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	
+	private ScriptContext context;
+	
 	ScheduledJob() {
 		logger.info("Initializing scheduled job");
+		
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("javascript");
+		engine.setBindings(getInitialBindings(engine), ScriptContext.ENGINE_SCOPE);
+		context = engine.getContext();
+		
+		InputStream in = this.getClass().getClassLoader().getResourceAsStream("tracker.js");
+		Reader reader = new InputStreamReader(in);
+		
+		try {
+			Object obj = engine.eval(reader, context);
+			
+		} catch (ScriptException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Bindings getInitialBindings(ScriptEngine engine) {
+		Bindings bindings = engine.createBindings();
+		bindings.put("console", logger);
+		return bindings;
 	}
 	
     protected void execute() throws JobExecutionException {
