b73adfb7e8b079b63b63b67f3a4e9cd4117258a2
Stuart Watt <stuart@morungos.com>
2015-09-03 14:50:57 -0400

    And a few more security unit tests

--- src/test/java/ca/uhnresearch/pughlab/tracker/security/LdapRealmTest.java
+++ src/test/java/ca/uhnresearch/pughlab/tracker/security/LdapRealmTest.java
@@ -3,13 +3,23 @@ package ca.uhnresearch.pughlab.tracker.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.replay;
+import com.nimbusds.jose.JOSEException;
+
+import static org.easymock.EasyMock.*;
 
 public class LdapRealmTest {
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
 	public void testConstructor() {
@@ -30,4 +40,44 @@ public class LdapRealmTest {
 		realm.setLdapContexts(contexts);
 	}
 
+	@Test
+	public void testDoGetAuthenticationInfoInvalidToken() {
+		
+		AuthenticationToken token = createMock(AuthenticationToken.class);
+		replay(token);
+		
+		LdapRealm realm = new LdapRealm();
+		
+		thrown.expect(AuthenticationException.class);
+
+		AuthenticationInfo result = realm.doGetAuthenticationInfo(token);
+		
+		Assert.assertNotNull(result);
+	}
+
+	@Test
+	public void testDoGetAuthenticationInfo() {
+		
+		LdapRealm realm = new LdapRealm();
+
+		UsernamePasswordToken token = createMock(UsernamePasswordToken.class);
+		replay(token);
+		
+		AuthenticationInfo info = createMock(AuthenticationInfo.class);
+		replay(info);
+		
+		LdapContext context = createMock(LdapContext.class);
+		expect(context.canAuthenticate(eq(token), eq(realm))).andStubReturn(true);
+		expect(context.query(eq(token), eq(realm))).andStubReturn(info);
+		replay(context);
+		
+		List<LdapContext> contexts = new ArrayList<LdapContext>();
+		contexts.add(context);
+		
+		realm.setLdapContexts(contexts);
+		
+		AuthenticationInfo result = realm.doGetAuthenticationInfo(token);
+		Assert.assertNotNull(result);
+		Assert.assertEquals(result, info);		
+	}
 }
