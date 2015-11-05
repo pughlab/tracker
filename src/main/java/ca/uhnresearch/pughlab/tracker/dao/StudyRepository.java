c46e9adec499849d3956c235130e8fb5dabcea83
Stuart Watt <stuart@morungos.com>
2015-11-05 14:24:08 -0500

    Javadoc improvements

--- src/main/java/ca/uhnresearch/pughlab/tracker/dao/StudyRepository.java
+++ src/main/java/ca/uhnresearch/pughlab/tracker/dao/StudyRepository.java
@@ -14,7 +14,7 @@ import ca.uhnresearch.pughlab.tracker.events.EventHandler;
 public interface StudyRepository {
 
 	/**
-	 * Retrieves all the studies from the repository
+	 * Retrieves all the studies from the repository.
 	 * @return list of studies
 	 */
 	List<Study> getAllStudies();
@@ -26,47 +26,49 @@ public interface StudyRepository {
 	Study getStudy(String name);
 	
 	/**
-	 * Writes or updates a study in the repository
+	 * Writes or updates a study in the repository.
 	 * @return a study
 	 */
 	Study saveStudy(Study name) throws RepositoryException;
 	
 	/**
-	 * Retrieves all the views for a study from the repository
+	 * Retrieves all the views for a study from the repository.
 	 * @return list of views
 	 */
 	List<View> getStudyViews(Study study);
 
 	/**
-	 * Writes the views for a study into the repository
+	 * Writes the views for a study into the repository.
+	 * @throws RepositoryException
 	 */
 	void setStudyViews(Study study, List<View> views) throws RepositoryException;
 
 	/**
-	 * Retrieves all the attributes for a study from the repository
+	 * Retrieves all the attributes for a study from the repository.
 	 * @return list of attributes
 	 */
 	List<Attributes> getStudyAttributes(Study study);
 
 	/**
-	 * Retrieves a single named attribute for a study from the repository
+	 * Retrieves a single named attribute for a study from the repository.
 	 * @return the attribute
 	 */
 	Attributes getStudyAttribute(Study study, String name);
 
 	/**
-	 * Writes the attributes for a study to the repository
+	 * Writes the attributes for a study to the repository.
+	 * @throws RepositoryException
 	 */
 	void setStudyAttributes(Study study, List<Attributes> attributes) throws RepositoryException;
 
 	/**
-	 * Retrieves a single specified view for a study from the repository
+	 * Retrieves a single specified view for a study from the repository.
 	 * @return a view
 	 */
 	View getStudyView(Study study, String name);
 
 	/**
-	 * Updates a single view for the study
+	 * Updates a single view for the study.
 	 * @param study
 	 * @param view
 	 * @throws RepositoryException
@@ -74,31 +76,32 @@ public interface StudyRepository {
 	void setStudyView(Study study, View view) throws RepositoryException;
 	
 	/**
-	 * Retrieves all the attributes for a view and study from the repository
+	 * Retrieves all the attributes for a view and study from the repository.
 	 * @return list of attributes
 	 */
 	List<ViewAttributes> getViewAttributes(Study study, View view);
 	
 	/**
-	 * Writes the attributes for a view and study to the repository
+	 * Writes the attributes for a view and study to the repository.
+	 * @throws RepositoryException
 	 */
 	void setViewAttributes(Study study, View view, List<ViewAttributes> attributes) throws RepositoryException;
 	
 	/**
-	 * Retrieves the record count for a specified study/view from the repository
+	 * Retrieves the record count for a specified study/view from the repository.
 	 * @return number of records
 	 */
 	Long getRecordCount(Study study, View view);
 		
 	/**
-	 * Retrieves the record-level data for a view and study from the repository
+	 * Retrieves the record-level data for a view and study from the repository.
 	 * @return list of JSON nodes
 	 */
 	List<ObjectNode> getCaseData(StudyCaseQuery query, View view);
 
 	/**
 	 * Retrieves the record-level data for a view and study from the repository,
-	 * filtered by a list of allowed attributes
+	 * filtered by a list of allowed attributes.
 	 * 
 	 * @return list of JSON nodes
 	 */
@@ -131,7 +134,7 @@ public interface StudyRepository {
 	
 	/**
 	 * Adds a new matching element to a study case filter. Only string values 
-	 * are typically allowed here, currently. A new study case query is returned
+	 * are typically allowed here, currently. A new study case query is returned.
 	 * and can be used later.
 	 * @param attribute
 	 * @param value
@@ -167,7 +170,7 @@ public interface StudyRepository {
 	StudyCaseQuery applyPager(StudyCaseQuery query, CasePager pager);
 
 	/**
-	 * Retrieves a single specified case for a study and view from the repository
+	 * Retrieves a single specified case for a study and view from the repository.
 	 * @return a case
 	 */
 	Cases getStudyCase(Study study, View view, Integer caseId);
@@ -187,16 +190,38 @@ public interface StudyRepository {
 	void setStudyCaseState(Study study, View view, Cases cases, String userName, String state);
 	
 	/**
-	 * Makes a new, empty, case. If an afterCase is passed, the new case will be added after that case
+	 * Makes a new, empty, case. The new case will be added after afterCase
 	 * and the others reordered. If afterCase is null or not passed, the new case will be added at the
-	 * beginning of the case list, which is very probably not what you want.
-	 * 
+	 * beginning of the case list, which is only sometimes what you want.
 	 * @return the case identifier
+	 * @throws RepositoryException
 	 */
 	Cases newStudyCase(Study study, View view, String userName, Cases afterCase) throws RepositoryException;
+	
+	/**
+	 * Makes a new, empty, case. The new case will be added at the
+	 * beginning of the case list, which is only sometimes what you want.
+	 * @param study
+	 * @param view
+	 * @param userName
+	 * @return
+	 * @throws RepositoryException
+	 */
 	Cases newStudyCase(Study study, View view, String userName) throws RepositoryException;
 	
+	/**
+	 * Selects which attributes will be returned in a query. 
+	 * @param query the current query
+	 * @param userName the current user's username
+	 * @param values
+	 * @return a list of CaseChangeInfo records
+	 * @throws RepositoryException
+	 */
 	List<CaseChangeInfo> setQueryAttributes(StudyCaseQuery query, String userName, ObjectNode values) throws RepositoryException;
 	
-	void setEventHandler(EventHandler manager);
+	/**
+	 * Sets the event handler.
+	 * @param handler the new event handler
+	 */
+	void setEventHandler(EventHandler handler);
 }
