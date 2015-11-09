598c9507465079ed60c27e9ad2a6917e83ef54d0
Stuart Watt <stuart@morungos.com>
2015-11-09 14:46:22 -0500

    Change the logout logic to fix the timing. Resolves #99

--- src/main/java/ca/uhnresearch/pughlab/tracker/security/AuditedSecurityManager.java
+++ src/main/java/ca/uhnresearch/pughlab/tracker/security/AuditedSecurityManager.java
@@ -9,8 +9,17 @@ import ca.uhnresearch.pughlab.tracker.dto.AuditLogRecord;
 
 public class AuditedSecurityManager extends DefaultWebSecurityManager {
 	
+	/**
+	 * Private field for the audit log repository.
+	 */
 	private AuditLogRepository auditLogRepository;
 	
+	/**
+	 * Handles login by additionally writing an audit record.
+	 * @param subject the subject
+	 * @param token the authentication token
+	 * @return the logged in subject
+	 */
 	@Override
 	public Subject login(Subject subject, AuthenticationToken token) {
 		Subject result = super.login(subject, token);
@@ -23,17 +32,24 @@ public class AuditedSecurityManager extends DefaultWebSecurityManager {
 		return result;
 	}
 	
+	/**
+	 * Handles logout by additionally writing an audit record.
+	 * @param subject the subject
+	 */
 	@Override 
 	public void logout(Subject subject) {
+		if (subject.getPrincipals().getPrimaryPrincipal() != null) {
+			String username = subject.getPrincipals().getPrimaryPrincipal().toString();
+			AuditLogRecord record = new AuditLogRecord();
+			record.setEventType("logout");
+			record.setEventUser(username);
+			auditLogRepository.writeAuditLogRecord(record);
+		}
 		super.logout(subject);		
-		
-		AuditLogRecord record = new AuditLogRecord();
-		record.setEventType("logout");
-		record.setEventUser(subject.getPrincipals().getPrimaryPrincipal().toString());
-		auditLogRepository.writeAuditLogRecord(record);
 	}
 
 	/**
+	 * Reads the audit log repository.
 	 * @return the auditLogRepository
 	 */
 	public AuditLogRepository getAuditLogRepository() {
@@ -41,6 +57,7 @@ public class AuditedSecurityManager extends DefaultWebSecurityManager {
 	}
 
 	/**
+	 * Sets the audit log repository.
 	 * @param auditLogRepository the auditLogRepository to set
 	 */
 	public void setAuditLogRepository(AuditLogRepository auditLogRepository) {
