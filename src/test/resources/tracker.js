bf6f4d9141f153ce307e3837488b4036fe454984
Stuart Watt <stuart@morungos.com>
2015-12-17 14:44:51 -0500

    Removed original field handling, as we don't do it with JS any more.
    This is because we now have a UI for this, and it's done through the event plugin system

--- src/test/resources/tracker.js
+++ src/test/resources/tracker.js
@@ -7,19 +7,19 @@ console.log("Welcome to the", console);
 // needed. But there's a lot to be said for using JS, as it allows study customizations and 
 // external logic. 
 
-events.get("DEMO").on("field", function(event) { 
-    var parameters = event.data.parameters;
-    console.log("Parameters", parameters);
-    var study = parameters.get("study").asText();
-    var field = parameters.get("field").asText();
-    var newValue = parameters.get("new").getValue();
-    if (field == "returnRequested") {
-        study = repository.getStudy(study);
-        var c = repository.getStudyCase(study, parameters.get("case_id").asInt());
-        var oldState = c.getState();
-        var newState = (newValue.toString() == "true") ? "pending" : null;
-        if (oldState != newState) {
-            repository.setStudyCaseState(study, c, event.data.user, newState);
-        }
-    }
-});
+//events.get("DEMO").on("field", function(event) { 
+//    var parameters = event.data.parameters;
+//    console.log("Parameters", parameters);
+//    var study = parameters.get("study").asText();
+//    var field = parameters.get("field").asText();
+//    var newValue = parameters.get("new").getValue();
+//    if (field == "returnRequested") {
+//        study = repository.getStudy(study);
+//        var c = repository.getStudyCase(study, parameters.get("case_id").asInt());
+//        var oldState = c.getState();
+//        var newState = (newValue.toString() == "true") ? "pending" : null;
+//        if (oldState != newState) {
+//            repository.setStudyCaseState(study, c, event.data.user, newState);
+//        }
+//    }
+//});
